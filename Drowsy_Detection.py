c3edf186479fdc0f47488e6d442f3a73042763c0
KhanhNguyen1308 <khanhnguyenduy1308@gmail.com>
2021-06-04 22:45:37 +0700

    update old and new file

--- Drowsy_Detection.py
+++ Drowsy_Detection.py
@@ -3,7 +3,7 @@ import time
 import mediapipe as mp
 from function import draw_point, eye_avg_ratio, put_text
 from head_pose_ratio import head_pose_ratio
-from head_pose_status import head_pose_x_status, head_pose_y_status, head_pose_z_status, eye_stat
+from head_pose_status import head_pose_status, eye_stat
 cap = cv2.VideoCapture(0)
 pTime = 0
 m = 0
